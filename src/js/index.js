abc8c218769bc7c32c8252b60a71e1c663ba13a0
daoanhhuy26012001 <daoanhhuy26012001@gmail.com>
2023-02-14 19:50:35 +0700

    add and show data

--- src/js/index.js
+++ src/js/index.js
@@ -1,24 +1,27 @@
+/*
 // example 2023/02/10
-// let root1, root2;
-// let a = prompt("Enter the first number: ");
-// let b = prompt("Enter the second number: ");
-// let c = prompt("Enter the third number: ");
-// let detal = b * b - 4 * a * c;
-// if (detal > 0) {
-//   root1 = (-b + Math.sqrt(detal)) / (2 * a);
-//   root2 = (-b - Math.sqrt(detal)) / (2 * a);
-//   console.log(`The roots of quadratic equation are ${root1} and ${root2}`);
-// } else if (detal == 0) {
-//   root1 = root2 = -b / (2 * a);
-//   console.log(`The roots of quadratic equation are ${root1} and ${root2}`);
-// } else {
-//   let realPart = (-b / (2 * a)).toFixed(2);
-//   let imagPart = (Math.sqrt(-detal) / (2 * a)).toFixed(2);
-//   console.log(
-//     `The roots of quadratic equation are ${realPart} + ${imagPart}i and ${realPart} - ${imagPart}i`
-//   );
-// }
+let root1, root2;
+let a = prompt("Enter the first number: ");
+let b = prompt("Enter the second number: ");
+let c = prompt("Enter the third number: ");
+let detal = b * b - 4 * a * c;
+if (detal > 0) {
+  root1 = (-b + Math.sqrt(detal)) / (2 * a);
+  root2 = (-b - Math.sqrt(detal)) / (2 * a);
+  console.log(`The roots of quadratic equation are ${root1} and ${root2}`);
+} else if (detal == 0) {
+  root1 = root2 = -b / (2 * a);
+  console.log(`The roots of quadratic equation are ${root1} and ${root2}`);
+} else {
+  let realPart = (-b / (2 * a)).toFixed(2);
+  let imagPart = (Math.sqrt(-detal) / (2 * a)).toFixed(2);
+  console.log(
+    `The roots of quadratic equation are ${realPart} + ${imagPart}i and ${realPart} - ${imagPart}i`
+  );
+}
+*/
 
+/*
 //Example 2023/02/13
 function ShowMessage(firstName)
 {
@@ -28,3 +31,17 @@ function ShowMessage(firstName)
     return SayHello();
 }
 ShowMessage("Steve");
+*/
+
+//Example 2023/02/14
+const student = {
+  firstName: "dao",
+  lastName: "huy",
+  position: "intern",
+  ages: 22,
+};
+
+const submit = document.getElementById("btn-primary");
+submit.addEventListener("click", () => {
+  console.log(student);
+});
