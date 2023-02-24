f8c2ba72913c4e93ab5f2e73a041522d8110df5d
daoanhhuy26012001 <daoanhhuy26012001@gmail.com>
2023-02-24 09:48:05 +0700

    complete lesson 3 & 4

--- src/js/index.js
+++ src/js/index.js
@@ -1,19 +1,35 @@
-// Lesson 1
+// Lesson 1 & 3
 const button = document.getElementById("jsstyle");
 
 button.addEventListener("click", () => {
   text.style.fontSize = "15px";
   text.style.fontFamily = "Roboto, sans-serif;";
   text.style.color = "green";
+  text.style.background = "black";
 });
 
 // Lesson 2
 function getFormValue() {
-  
-  var form = document.getElementById('form1')
+  var form = document.getElementById("form1");
   for (var i = 0; i < form.length; i++) {
-    if (form.elements[i].value != 'Submit') {
+    if (form.elements[i].value != "Submit") {
       alert(form.elements[i].value);
     }
   }
 }
+
+// Lesson 4
+const btn = document.getElementById("getAttributes");
+
+btn.addEventListener("click", () => {
+  var h = document.getElementById("w3r").href;
+  alert("href : " + h);
+  var t = document.getElementById("w3r").target;
+  alert("target : " + t);
+  var r = document.getElementById("w3r").rel;
+  alert("rel : " + r);
+  var hl = document.getElementById("w3r").hreflang;
+  alert("hreflang : " + hl);
+  var tp = document.getElementById("w3r").type;
+  alert("type : " + tp);
+});
